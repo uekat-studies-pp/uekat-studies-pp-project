35387766acc458d84548691b5adde6326243056a
rbrauner <rafalbrauner@gmail.com>
2023-06-08 13:04:08 +0200

    steam scrapper

--- scrapper/cron.py
+++
@@ -1,9 +0,0 @@
-from crontab import CronTab
-import os
-
-script_file = os.getcwd() + "/main.py"
-
-cron = CronTab(user='root')
-job = cron.new(command='python ' + script_file)
-job.second.every(1)
-cron.write()
